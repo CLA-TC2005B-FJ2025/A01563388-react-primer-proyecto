0e6c4c68d8ee0d53f6cc6c64350eae99d237beab
Alonso Alejandro Alarcón Parra <155941412+alxxjandro@users.noreply.github.com>
2025-03-28 16:09:33 +0000

    agregue bulma

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css'
 import React from 'react'
 import images from './images'
 import { Tarjeta } from './Tarjeta'
@@ -5,10 +6,23 @@ import { Tarjeta } from './Tarjeta'
 export function App() {
   return (
     <div>
-        <div><h1>Personajes de Mario Bros</h1></div>
-        <Tarjeta nombre = "Mario" colorFav="Rojo" img={images.Mario}/>
-        <Tarjeta nombre = "Luigi" colorFavor="Verde" img={images.Luigi}/>
-        <Tarjeta nombre = "Peaches" colorFav="Pink" img={images.Peaches}/>
+        <div>Personajes de Mario Bros</div>
+        <div className="container">
+          <section className="section">
+            <div className="columns">
+              <div className="column is-2">
+                <Tarjeta nombre="Mario" colorFavorito="Rojo" image={images.Mario}/>
+              </div>
+              <div className="column is-2">
+                <Tarjeta nombre="Luigi" colorFavorito="Verde" image={images.Luigi}/>
+              </div>
+              <div className="column is-2">
+                <Tarjeta nombre="Peaches" colorFavorito="Rosa" image={images.Peaches}/>
+              </div>
+            </div>
+          </section>
+        </div>
     </div>
+  
   )
 }
--- src/Tarjeta.js
+++ src/Tarjeta.js
@@ -1,12 +1,21 @@
 import React from 'react'
 
-export function Tarjeta(props) { 
+export function Tarjeta( props ) {  // se le puede poner otro nombre
   return (
-    <div>
-        <h3>Nombre:{props.nombre} </h3>
-        <h3>Color Favorito: {props.colorFav} </h3>
-        <img src={props.img} alt={props.nombre} />
-        <br></br>
-    </div>
+    <div className="card">
+        <div className="card-image">
+          <figure className="image is-1by1">
+            <img src={props.image} alt="pda logo" />
+          </figure>
+        </div>
+  
+        <div className="card-content">
+          <div className="media-content">
+            <p className="title is-4">{props.nombre}</p>
+            <p className="subtitle is-6">{props.colorFavorito} es mi color favorito</p>
+          </div>
+        </div>
+      </div>
+  
   )
 }
